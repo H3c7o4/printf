f6ff52ba1f9fa0c73178327712191482fb0b6588
CynthiaAdesuwa <cynthiaadesuwaosagie@gmail.com>
2022-10-16 10:28:36 -0700

    binary_print.c

---
+++ main.h
@@ -0,0 +1,34 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int _printf(const char *format, ...);
+int _position(const char *s, int n);
+int _strlen(char *s);
+char *_strcat(char *dest, char *src, int n);
+int _abs(int n);
+int _numlen(int n);
+void *rev_string(char *s);
+
+/**
+ * struct type - Struct data type
+ *
+ * @op: data type argument
+ * @f: The function associated
+ */
+typedef struct type
+{
+	char *op;
+	char *(*f)(va_list);
+}
+type_t;
+
+char *print_c(va_list list);
+char *print_s(va_list list);
+char *print_i(va_list list);
+char *print_bin(va_list list);
+
+#endif /*MAIN_H*/
